9b3be69b2a429e79b8cb6c28bfaf48b3e5926441
ramesh.naidu <kundetirameshnaidu3@gmail.com>
2023-01-28 12:49:59 +0530

    ffffffwweew

--- PracticeJan/src/main/java/Sample11Token.java
+++ PracticeJan/src/main/java/Sample11Token.java
@@ -4,7 +4,7 @@ public class Sample11Token {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("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");
-
+		System.out.println("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");
 	}
 
 }
